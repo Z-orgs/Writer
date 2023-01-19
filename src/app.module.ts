aa301ca79029e184c25dc2d3c2e14380a69dde29
REdirect <60683892+VuongSyHanh@users.noreply.github.com>
2023-01-19 09:37:46 +0700

    delete almost, do it again later

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostModule } from './post/post.module';
 import 'dotenv/config';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserController } from './user/controller/user.controller';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    PostModule,
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-    }),
-    UserModule,
-  ],
-  controllers: [AppController, UserController],
+  imports: [],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
