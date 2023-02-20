4d570fd318a1423b3535497f51412648478d1d85
Tom <60683892+VuongSyHanh@users.noreply.github.com>
2023-02-20 17:57:15 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
+(async function () {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
 	await app.listen(3000);
-}
-bootstrap();
+})();
